9c7372b1702495a7da97b3c21dd0f9bfead15d92
wxm <157215725@qq.com>
2025-08-21 16:00:25 +0800

    fix: optional yoga-layout

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       entry: resolvePath('./src/index.ts'),
     },
     rollupOptions: {
-      external: ['modern-gif', 'lottie-web', 'yoga-layout'],
+      external: ['modern-gif', 'lottie-web', /^yoga-layout/],
       output: [
         {
           format: 'umd',
