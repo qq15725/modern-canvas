75f00f257f5856e814047a33f0a0648a1072e41a
wxm <157215725@qq.com>
2025-10-23 17:42:04 +0800

    feat: render support keyframes

--- src/render.ts
+++ src/render.ts
@@ -7,9 +7,14 @@ export interface RenderOptions {
   width: number
   height: number
   debug?: boolean
-  time?: number
   fonts?: Fonts
-  onBeforeRender?: (engine: Engine) => void | Promise<void>
+  keyframes?: number[]
+  onBefore?: (engine: Engine) => void | Promise<void>
+  onFrame?: (frame: Uint8ClampedArray, ctx: {
+    currentTime: number
+    duration: number
+    progress: number
+  }) => Promise<void>
 }
 
 let engine: Engine | undefined
@@ -39,17 +44,29 @@ async function start(sleep = 100): Promise<void> {
 }
 
 async function task(options: RenderOptions): Promise<HTMLCanvasElement> {
-  const { debug = false, fonts, width, height, time = 0, data } = options
+  const {
+    debug = false,
+    fonts,
+    width,
+    height,
+    data,
+    keyframes = [],
+    onBefore,
+    onFrame,
+  } = options
+
   engine ??= new Engine({
     pixelRatio: 1,
     width: 1,
     height: 1,
     preserveDrawingBuffer: true,
   })
+
+  // reset
   engine.debug = debug
   engine.fonts = fonts
+  engine.timeline.currentTime = 0
   engine.root.removeChildren()
-  engine.timeline.currentTime = time
   engine.resize(width, height, true)
   ;(Array.isArray(data) ? data : [data]).forEach((v) => {
     if (v instanceof Node) {
@@ -60,8 +77,32 @@ async function task(options: RenderOptions): Promise<HTMLCanvasElement> {
       engine!.root.appendChild(Node.parse(v) as unknown as Node)
     }
   })
-  await options.onBeforeRender?.(engine)
+
+  // render
+  await onBefore?.(engine)
   await engine.waitAndRender()
+  await new Promise<void>((resolve) => {
+    let i = 0
+    const len = keyframes.length
+    const last = keyframes[len - 1]
+    async function handle(): Promise<void> {
+      if (i === len)
+        return resolve()
+      const currentTime = keyframes[i++]
+      const next = keyframes[i] || currentTime
+      const duration = next - currentTime
+      engine!.timeline.currentTime = currentTime
+      engine!.render()
+      await onFrame?.(engine!.toPixels(), {
+        currentTime,
+        duration,
+        progress: currentTime / last,
+      })
+      requestAnimationFrame(handle)
+    }
+    handle()
+  })
+
   return engine.toCanvas2D()
 }
 
