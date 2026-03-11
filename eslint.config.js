7e8bde391c3ef5d565c4f596e787b8547071174b
wxm <157215725@qq.com>
2026-03-11 18:39:48 +0800

    feat: up deps

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,13 @@ import antfu from '@antfu/eslint-config'
 export default antfu(
   {
     type: 'lib',
+    e18e: false,
   },
   {
     rules: {
-      'antfu/consistent-list-newline': ['off'],
-      'ts/no-unsafe-declaration-merging': ['off'],
-      'eslint-comments/no-unlimited-disable': ['off'],
+      'antfu/consistent-list-newline': 'off',
+      'ts/no-unsafe-declaration-merging': 'off',
+      'eslint-comments/no-unlimited-disable': 'off',
     },
   },
 )
